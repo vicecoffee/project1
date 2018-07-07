847e195e25b97c4899d84abdb9e2fad0010bb773
Valerie Yi Coffee <vcoffee@hsph.harvard.edu>
2018-07-07 22:28:05 +0000

    Added register/login/logout

--- application.py
+++ application.py
@@ -29,11 +29,83 @@ def index():
 
 @app.route("/register")
 def register():
-    return
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 400)
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = :username",
+                          username=request.form.get("username"))
+
+        # Ensure username is unique
+        if len(rows) >= 1:
+            return apology("username already exists", 400)
+
+        # Ensure password was submitted
+        if not request.form.get("password"):
+            return apology("must provide password", 400)
+
+        # Ensure password and confirmation match
+        if request.form.get("confirmation") != request.form.get("password"):
+            return apology("must match password and confirm", 400)
+
+        # Insert username into db
+        db.execute("INSERT INTO users (username, hash) VALUES (:username, :hashword)",
+                   username=request.form.get("username"), hashword=generate_password_hash(request.form.get("password")))
+
+        # Get user info for new user
+        rows = db.execute("SELECT * FROM users WHERE username = :username",
+                          username=request.form.get("username"))
+
+        # Log in user automatically.  Store id in session
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("register.html")
 
 @app.route("/login")
 def login():
-    return
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = :username",
+                          username=request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
 
 @app.route("/logout")
 def logout():
