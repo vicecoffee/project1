0132d331d78bd3870315f19fc8b8ee4e1264cba8
Valerie Yi Coffee <vcoffee@hsph.harvard.edu>
2018-07-07 17:59:47 +0000

    Added some files

---
+++ import.py
@@ -0,0 +1,12 @@
+#to import csv files
+import csv
+from cs50 import SQL
+
+db= SQL("postgres://vqhlwxbowulgoc:b44290dbb0843fe951312cf0dfa960092dd6c84ddf11dd105f0f01796e1cc688@ec2-23-23-220-19.compute-1.amazonaws.com:5432/d888fp495haud5")
+f= open("zips.csv")
+reader = csv.reader(f)
+# https://stackoverflow.com/questions/11349333/when-processing-csv-data-how-do-i-ignore-the-first-line-of-data
+next(reader)
+# I tested the import with a dummy command and it populated until 318. I deleted those entries.
+for row in reader:
+	db.execute("INSERT INTO locations (zipcode, town, state, latitude, longitude, population) VALUES (:zipcode, :town, :state, :latitude, :longitude, :population)", zipcode=row[0], town=row[1], state=row[2], latitude=row[3], longitude=row[4], population=row[5])
